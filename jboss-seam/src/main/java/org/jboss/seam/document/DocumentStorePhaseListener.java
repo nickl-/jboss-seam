c80dd6859b67e54a297c774f79fb8a95c1e44a5d
alberto <infjaf@gmail.com>
2016-02-14 17:09:19 +0100

    Fix for JBSEAM-4853: Error downloading documents in IE over SSL

--- jboss-seam/src/main/java/org/jboss/seam/document/DocumentStorePhaseListener.java
+++ jboss-seam/src/main/java/org/jboss/seam/document/DocumentStorePhaseListener.java
@@ -6,6 +6,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jboss.seam.log.LogProvider;
@@ -51,10 +52,11 @@ public class DocumentStorePhaseListener implements PhaseListener
          {
 
             HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+            HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
             response.setContentType(documentData.getDocumentType().getMimeType());
 
             response.setHeader("Content-Disposition", documentData.getDisposition() + "; filename=\"" + documentData.getFileName() + "\"");
-
+            setHeadersForInternetExplorer(request, response);
             documentData.writeDataToStream(response.getOutputStream());
             context.responseComplete();
          }
@@ -64,5 +66,20 @@ public class DocumentStorePhaseListener implements PhaseListener
          log.warn(e);
       }
    }
-
+   private void setHeadersForInternetExplorer(HttpServletRequest request, HttpServletResponse response){
+       if (request == null) {
+           return;
+       }
+       if (request.isSecure() && isIE(request)){
+           response.setHeader("Pragma", "a");
+           response.setHeader("Cache-Control", "max-age=0");
+           response.addHeader("Cache-Control", "must-revalidate");
+       }
+   }
+   private boolean isIE (HttpServletRequest request) {        
+       assert request != null;
+       String useragent = request.getHeader("User-Agent");
+       boolean isIE = useragent != null && useragent.contains("MSIE");
+       return isIE;
+   }
 }
