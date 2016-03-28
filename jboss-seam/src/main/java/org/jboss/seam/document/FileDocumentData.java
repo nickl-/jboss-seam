a9ec1451ff8630dec9241e71f1e2fea73454499a
albfernandez <infjaf@gmail.com>
2016-03-28 18:57:50 +0200

    Serves DocumentData on DocumentStore from a file

---
+++ jboss-seam/src/main/java/org/jboss/seam/document/FileDocumentData.java
@@ -0,0 +1,44 @@
+package org.jboss.seam.document;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileDocumentData extends DocumentData {
+
+	private File data;
+
+	public FileDocumentData(String baseName, DocumentType documentType, File data) {
+		super(baseName, documentType);
+		this.data = data;
+	}
+
+	@Override
+	public void writeDataToStream(OutputStream stream) throws IOException {
+		InputStream is = null;
+		try {
+			is = new BufferedInputStream(new FileInputStream(data));
+			byte[] buffer = new byte[4096];
+			int read = 0;
+			while ((read = is.read(buffer)) > 0) {
+				stream.write(buffer, 0, read);
+			}
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException ioe) {
+					//
+				}
+			}
+		}
+	}
+
+	public File getData() {
+		return this.data;
+	}
+
+}
